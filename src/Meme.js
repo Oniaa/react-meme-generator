ba7ee94fd9916c25f53434d2a3e7c4a31622e8a1
ony <ogbuebele.ony@hotmail.com>
2023-05-04 17:38:04 +0200

    Finish Form

--- src/Meme.js
+++ src/Meme.js
@@ -8,11 +8,35 @@ export default function Meme() {
   const [randomImage, setRandomImage] = useState(
     'https://api.memegen.link/images/buzz/memes/memes_everywhere.gif',
   );
+  const preventSubmit = (event) => {
+    event.preventDefault();
+  };
+
+  const handleChange = (event) => {
+    setInputText({
+      ...inputText,
+      [event.currentTarget.name]: event.currentTarget.value,
+    });
+  };
+
   return (
     <div className="meme-container">
-      <form>
-        <input name="topText" />
-        <input name="bottomText" />
+      <form onSubmit={preventSubmit}>
+        <label htmlFor="topText">Top Text</label>
+        <input
+          id="topText"
+          name="topText"
+          value={inputText.topText}
+          onChange={handleChange}
+        />
+        <label htmlFor="bottomText">Bottom Text</label>
+        <input
+          id="bottomText"
+          name="bottomText"
+          value={inputText.bottomText}
+          onChange={handleChange}
+        />
+        <button>Generate Meme</button>
       </form>
     </div>
   );
